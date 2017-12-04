a740f9539739d3c310ba6b0b9e6a7a270b84026a
Stephen <wilkins.c.stephen@gmail.com>
2017-12-04 00:52:45 -0500

    Completed CRC functionality and example code

--- emulator/TestFiles/CRCtest.c
+++ emulator/TestFiles/CRCtest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "xcustom.h"
+#include <inttypes.h>
 
 #define k_UPDATE 0
 #define k_CALC 1
@@ -9,16 +10,19 @@
 
 int main() {
   printf("Running CRC\n");
-	uint16_t data [] = {0xdead, 0xbeef, 0x0bad, 0xf00d}, y;
-	uint32_t taps = 0xD5;
-	uint32_t length = 16;
+  //uint16_t data [] = {0xdead, 0xbeef, 0x0bad, 0xf00d}, y;
+  uint64_t data [] = {0xdeadbeef0badf00d}, y;
+	uint32_t taps = 0x814141AB;
+	uint32_t length = 32;
+  uint32_t memlen = 64;
 
-	uint16_t crc = 0xF;
-    printf("Taps start:  %zu\n", taps);
-  	crcUpdate(y, 0xD5, 8);
-    printf("Should be F:  %zu\n", crc);
-  	crcCalc(crc, &data, 16);
-    printf("CRC:  %zu\n", crc);
-  	printf("Test:  %zu\n", y);
+	uint64_t crc = 0;
+  printf("CRC polynomial:  %lu\n", taps);
+  printf("CRC size:  %lu\n", length);
+  printf("CRC calculated across: %llu\n", memlen);
+	crcUpdate(y, taps, length);
+  printf("CRC variable initial state:  %lu\n", crc);
+	crcCalc(crc, &data, memlen);
+  printf("CRC variable final state:  %lu\n", crc);
 	return 0;
 }
--- src/main/scala/tile/CRC.scala
+++ src/main/scala/tile/CRC.scala
@@ -11,12 +11,10 @@ import freechips.rocketchip.util.InOrderArbiter
 
 class CRCAccelerator(implicit p: Parameters) extends LazyRoCC {
   override lazy val module = new CRCAcceleratorModule(this)
-  override val atlNode = TLClientNode(Seq(TLClientPortParameters(Seq(TLClientParameters("")))))
 }
 
-class CRCAcceleratorModule(outer: CRCAccelerator) extends LazyRoCCModule(outer)
-with HasCoreParameters
-with HasL1CacheParameters {
+class CRCAcceleratorModule(outer: CRCAccelerator)(implicit p: Parameters) extends LazyRoCCModule(outer)
+with HasCoreParameters {
   // The parts of the command are as follows
   // inst - the parts of the instruction itself
   //   opcode
@@ -29,19 +27,12 @@ with HasL1CacheParameters {
   //   xs2 - is the second source register being used?
   // rs1 - the value of source register 1
   // rs2 - the value of source register 2
-
-  // Used to determine if updating length/type or CRC calculation
-  val cacheParams = tileParams.icache.get
-
-  // Cache block offset, used for address calculation
-  private val blockOffset = blockOffBits 
-
   val resp_rd = Reg(io.resp.bits.rd)
 
   // The 32 bits that determine the location of the XOR taps
   val taps = Reg(Vec(32, Bool()))
 
-  val length = Reg(UInt(log2Up(32)))
+  val length = Reg(UInt(width = 10))
   // The bitmask that determines the final CRC length
   val outMask = Reg(UInt(width = 32))
   // The memory location to read for CRC calculation
@@ -51,21 +42,10 @@ with HasL1CacheParameters {
   // The current global data offset (in bits)
   val globalCount = Reg(UInt(width = 32))
   // The current block data offset (in bits)
-  val localIndex = Reg(UInt(width = log2Up(cacheDataBits)))
+  val localIndex = Reg(UInt(width = 16))
 
   val crcState = Reg(Vec(32, Bool()))
-
-  val addr_block = memsrc(coreMaxAddrBits - 1, blockOffset)
-  val offset = memsrc(blockOffset - 1, 0)
-  val next_addr = (addr_block + UInt(1)) << UInt(blockOffset)
-
-  // Used to read memory for CRC calculation
-  val (tl_out, edgesOut) = outer.atlNode.out(0)
-  // The cache line
-  val recv_data = Reg(UInt(width = cacheDataBits))
-
-
-  
+  val recv_data = Reg(UInt(width = 64)) //Get 64 bits per load
 
   val s_idle :: s_update :: s_acq :: s_recv :: s_calc :: s_zeroes:: s_resp :: Nil = Enum(Bits(), 7)
   //  Idle, update len, acq data, recv data, calc CRC, responding
@@ -75,117 +55,99 @@ with HasL1CacheParameters {
   io.cmd.ready := (state === s_idle)
   // Cmd response is valid when in response state
   io.resp.valid := (state === s_resp)
+  io.resp.bits.data := Reverse(Cat(crcState))
   io.resp.bits.rd := resp_rd
-  io.resp.bits.data := Cat(crcState)
 
-  // Request is valid in the acq state
-  tl_out.a.valid := (state === s_acq)
-  // Mem address
-  tl_out.a.bits := edgesOut.Get(
-                       fromSource = UInt(0),
-                       toAddress = addr_block << blockOffset,
-                       lgSize = UInt(lgCacheBlockBytes))._2
-  // Receive is valid in the recv state
-  tl_out.d.ready := (state === s_recv)
 
   // Command received from CPU
   when (io.cmd.fire()) {
-    resp_rd := io.cmd.bits.inst.rd
     // Funct of 0 indicate updating type/length
+    resp_rd := io.cmd.bits.inst.rd
     when (io.cmd.bits.inst.funct === UInt(0)) {
-      when (io.cmd.bits.rs1 === UInt(0)) {
-        // Is update state actually needed?
-        // If we're doing hardcoded type checks, yes
-        state := s_update
-      } .otherwise {
-        length := io.cmd.bits.rs2
-        when (length < UInt(32)) {
-          outMask := ~(Fill(32, Bits("b1")) << length)
-        }
-        taps(31) := true.B
-        val shifted = (io.cmd.bits.rs1) << (UInt(32) - length)
-        for (i <- 0 until 30) {
-          taps(i) := shifted(i)
-        }
-        state := s_resp // Have to do this to get the processor to recognize that the update is done
-        //taps := (io.cmd.bits.rs1) << (UInt(32) - length)
+      // Update taps for CRC polynomial
+      val lengthInput = io.cmd.bits.rs2(9, 0)
+      length := lengthInput
+      when (lengthInput < UInt(33)) {
+        outMask := ~(Fill(32, true.B) << lengthInput)
       }
+      val shiftAmt = UInt(32) - lengthInput
+      val shifted = io.cmd.bits.rs1 << shiftAmt
+      for (i <- 0 to 31) {
+        taps(i) := shifted(i)
+      }
+      /*for (i <- 10 to 31) {
+        taps(i) := false.B
+      }*/
+      state := s_resp
+      //taps := (io.cmd.bits.rs1) << (shiftAmt)
     } .otherwise {
       memsrc := io.cmd.bits.rs1
-      resp_rd := io.cmd.bits.inst.rd
       memlen := io.cmd.bits.rs2
-      crcState := Vec.fill(32){Bool(false)}
-      //for (i <- 0 until 31) {
-      //  crcState(i) := false.B
-      //}
 
-      //crcState := Vec(32, false.B)
       globalCount := UInt(0)
-
+      crcState := Vec.fill(32){false.B}
       // Should be good to go with the memory
       state := s_acq
     }
   }
 
-  // Update taps/length
-  when (state === s_update) {
-    when (Cat(taps) === UInt(0)) {
-      // TODO:  Set taps & mask based on table of predefined types
-    }
-    state := s_idle
-  }
-
   // Memory request sent
-  when (tl_out.a.fire()) {
-    state := s_recv
+  when (state === s_acq) {
+    when (io.mem.req.fire()) {
+      state := s_recv
+    }
   }
 
   // Memory request received
-  when (tl_out.d.fire()) {
-    recv_data := tl_out.d.bits.data
-    // Fix the bit index
-    localIndex := UInt(cacheDataBits) - (offset * UInt(8))
-    state := s_calc
+  when (state === s_recv) {
+    when (io.mem.resp.valid) {
+      recv_data := io.mem.resp.bits.data
+      localIndex := UInt(64)
+      state := s_calc
+    }
   }
 
   when (state === s_calc) {
-    when (globalCount === length) {
+    when (globalCount === memlen) {
       localIndex := UInt(0)
       state := s_zeroes
     } .elsewhen (localIndex === UInt(0)) {
-      memsrc := next_addr
+      memsrc := memsrc + UInt(8)
       state := s_acq
     } .otherwise {
       //TODO:  Calculate CRC
       //Use localIndex - 1 because localIndex = 0 is the escape from the current block
       val dataBit = recv_data(localIndex - UInt(1))
       val finalBit = crcState(31)
-      for (i <- 1 until 31) {
+      
+      for (i <- 1 to 31) {
         val bitXOR = crcState(i - 1) ^ finalBit
         crcState(i) := Mux(taps(i), bitXOR, crcState(i - 1))
       }
       val bitXOR = dataBit ^ finalBit
       crcState(0) := Mux(taps(0), bitXOR, dataBit)
-
+      /*for (i <- 0 to 31) {
+        crcState(i) := recv_data(i)
+      }*/;
       localIndex := localIndex - UInt(1)
       globalCount := globalCount + UInt(1)
     }
   }
 
   when (state === s_zeroes) {
-    when (localIndex >= length) {
+    when (localIndex >= UInt(32)) {
       //val masked = Cat(crcState) & outMask
       val shiftAmt = UInt(32) - length
-      val shifted = Cat(crcState) >> shiftAmt
+      val shifted = Reverse(Cat(crcState)) >> shiftAmt
       val masked = shifted & outMask
-      for (i <- 0 until 31) {
-        crcState(i) := masked(i)
+      for (i <- 0 to 31) {
+        crcState(i) := shifted(i)  
       }
       state := s_resp
     } .otherwise {
       val dataBit = UInt(0)
       val finalBit = crcState(31)
-      for (i <- 1 until 31) {
+      for (i <- 1 to 31) {
         val bitXOR = crcState(i - 1) ^ finalBit
         crcState(i) := Mux(taps(i), bitXOR, crcState(i - 1))
       }
@@ -202,9 +164,12 @@ with HasL1CacheParameters {
 
   io.busy := (state =/= s_idle)
   io.interrupt := Bool(false)
-  io.mem.req.valid := Bool(false)
-  // Tie off unused channels
-  tl_out.b.ready := Bool(true)
-  tl_out.c.valid := Bool(false)
-  tl_out.e.valid := Bool(false)
+  io.mem.req.valid := (state === s_acq)
+  io.mem.req.bits.addr := memsrc
+  io.mem.req.bits.tag := memsrc(5, 0)
+  io.mem.req.bits.cmd := M_XRD // perform a load (M_XWR for stores)
+  io.mem.req.bits.typ := MT_D // D = 8 bytes, W = 4, H = 2, B = 1
+  io.mem.req.bits.data := Bits(0) // we're not performing any stores...
+  io.mem.req.bits.phys := Bool(false)
+  io.mem.invalidate_lr := Bool(false)
 }
